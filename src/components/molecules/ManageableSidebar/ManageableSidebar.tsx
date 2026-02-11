e2a5d10729539093ff774cdf8394b7a9781e1a4a
typescreep <typescreep@prorobotech.ru>
2026-02-11 19:05:56 +0300

    sidebar reqsjsonpaths

--- src/components/molecules/ManageableSidebar/ManageableSidebar.tsx
+++ src/components/molecules/ManageableSidebar/ManageableSidebar.tsx
@@ -1,14 +1,26 @@
+/* eslint-disable no-nested-ternary */
 import React, { FC, useState, useEffect } from 'react'
 import { theme as antdtheme } from 'antd'
 import { useLocation, useParams } from 'react-router-dom'
-import { ManageableSidebarProvider } from '@prorobotech/openapi-k8s-toolkit'
+import { ManageableSidebarProvider, useK8sSmartResource } from '@prorobotech/openapi-k8s-toolkit'
+import type { TSingleResource } from '@prorobotech/openapi-k8s-toolkit'
 import { useSelector } from 'react-redux'
 import type { RootState } from 'store/store'
 import { HEAD_FIRST_ROW, SELECTOR_CLUSTER_NEW_HEIGHT } from 'constants/blocksSizes'
 import {
   BASE_API_GROUP,
   BASE_API_VERSION,
+  BASE_INSTANCES_API_GROUP,
+  BASE_INSTANCES_API_VERSION,
+  BASE_INSTANCES_PLURAL,
+  BASE_PROJECTS_API_GROUP,
+  BASE_PROJECTS_API_VERSION,
+  BASE_PROJECTS_PLURAL,
+  BASE_USE_NAMESPACE_NAV,
   BASE_USE_NEW_NAVIGATION,
+  CUSTOM_NAMESPACE_API_RESOURCE_API_GROUP,
+  CUSTOM_NAMESPACE_API_RESOURCE_API_VERSION,
+  CUSTOM_NAMESPACE_API_RESOURCE_PLURAL,
   CUSTOMIZATION_SIDEBAR_FALLBACK_ID,
 } from 'constants/customizationApiGroupAndVersion'
 import { Styled } from './styled'
@@ -34,6 +46,62 @@ export const ManageableSidebar: FC<TManageableSidebarProps> = ({
   const syntheticProject = params?.syntheticProject || ''
   const theme = useSelector((state: RootState) => state.openapiTheme.theme)
   const { token } = antdtheme.useToken()
+  const isNamespaceNavigation = BASE_USE_NAMESPACE_NAV === 'true'
+  const req0Name = syntheticProject || namespace
+
+  const isCustomNamespaceResource =
+    CUSTOM_NAMESPACE_API_RESOURCE_API_GROUP &&
+    typeof CUSTOM_NAMESPACE_API_RESOURCE_API_GROUP === 'string' &&
+    CUSTOM_NAMESPACE_API_RESOURCE_API_GROUP.length > 0 &&
+    CUSTOM_NAMESPACE_API_RESOURCE_API_VERSION &&
+    typeof CUSTOM_NAMESPACE_API_RESOURCE_API_VERSION === 'string' &&
+    CUSTOM_NAMESPACE_API_RESOURCE_API_VERSION.length > 0 &&
+    CUSTOM_NAMESPACE_API_RESOURCE_PLURAL &&
+    typeof CUSTOM_NAMESPACE_API_RESOURCE_PLURAL === 'string' &&
+    CUSTOM_NAMESPACE_API_RESOURCE_PLURAL.length > 0
+
+  const { data: projectData } = useK8sSmartResource<{ items: TSingleResource[] }>({
+    cluster,
+    apiGroup: BASE_PROJECTS_API_GROUP,
+    apiVersion: BASE_PROJECTS_API_VERSION,
+    plural: BASE_PROJECTS_PLURAL,
+    fieldSelector: `metadata.name=${req0Name}`,
+    isEnabled: Boolean(cluster && req0Name && !isNamespaceNavigation),
+  })
+
+  const { data: namespaceBuiltinData } = useK8sSmartResource<{ items: TSingleResource[] }>({
+    cluster,
+    apiVersion: 'v1',
+    plural: 'namespaces',
+    fieldSelector: `metadata.name=${req0Name}`,
+    isEnabled: Boolean(cluster && req0Name && isNamespaceNavigation && !isCustomNamespaceResource),
+  })
+
+  const { data: namespaceCustomData } = useK8sSmartResource<{ items: TSingleResource[] }>({
+    cluster,
+    apiGroup: CUSTOM_NAMESPACE_API_RESOURCE_API_GROUP,
+    apiVersion: CUSTOM_NAMESPACE_API_RESOURCE_API_VERSION,
+    plural: CUSTOM_NAMESPACE_API_RESOURCE_PLURAL,
+    fieldSelector: `metadata.name=${req0Name}`,
+    isEnabled: Boolean(cluster && req0Name && isNamespaceNavigation && isCustomNamespaceResource),
+  })
+
+  const { data: instanceData } = useK8sSmartResource<{ items: TSingleResource[] }>({
+    cluster,
+    apiGroup: BASE_INSTANCES_API_GROUP,
+    apiVersion: BASE_INSTANCES_API_VERSION,
+    plural: BASE_INSTANCES_PLURAL,
+    namespace: syntheticProject || undefined,
+    fieldSelector: `metadata.name=${namespace}`,
+    isEnabled: Boolean(cluster && namespace && syntheticProject),
+  })
+
+  const currentReq0Resource = isNamespaceNavigation
+    ? isCustomNamespaceResource
+      ? namespaceCustomData?.items?.[0]
+      : namespaceBuiltinData?.items?.[0]
+    : projectData?.items?.[0]
+  const currentInstanceResource = instanceData?.items?.[0]
 
   const [height, setHeight] = useState(0)
 
@@ -89,6 +157,19 @@ export const ManageableSidebar: FC<TManageableSidebarProps> = ({
         fallbackIdToCompare={
           namespace ? `${CUSTOMIZATION_SIDEBAR_FALLBACK_ID}-namespaced` : CUSTOMIZATION_SIDEBAR_FALLBACK_ID
         }
+        multiQueryData={{
+          req0: currentReq0Resource || (req0Name ? { metadata: { name: req0Name } } : undefined),
+          req1:
+            currentInstanceResource ||
+            (syntheticProject && namespace
+              ? {
+                  metadata: {
+                    name: namespace,
+                    namespace: syntheticProject,
+                  },
+                }
+              : undefined),
+        }}
         currentTags={currentTags}
         noMarginTop
       />
