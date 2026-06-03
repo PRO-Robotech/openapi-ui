6b2999c9908251625f6503ecb64c45514553ea63
typescreep <typescreep@prorobotech.ru>
2026-06-03 13:20:10 +0300

    new resource badges

--- src/components/organisms/Search/molecules/SearchEntry/styled.ts
+++ src/components/organisms/Search/molecules/SearchEntry/styled.ts
@@ -27,11 +27,22 @@ type TAbbrProps = {
 
 const Abbr = styled.span<TAbbrProps>`
   background-color: ${({ $bgColor }) => $bgColor};
-  border-radius: 13px;
-  padding: 1px 5px;
-  font-size: 13px;
-  height: min-content;
-  margin-right: 4px;
+  border: 1px solid ${({ $bgColor }) => $bgColor};
+  border-radius: 4px;
+  padding: 0 7px;
+  height: 22px;
+  font-family: 'SF Pro', sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  letter-spacing: 0;
+  box-sizing: border-box;
+  flex-shrink: 0;
 `
 
 type TApiGroupVersionProps = {
